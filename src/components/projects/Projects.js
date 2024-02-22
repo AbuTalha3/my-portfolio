234b99913c9ef53340cc876880489479f6320280
Abu Talha (Najeeb Uddin) <127955219+AbuTalha3@users.noreply.github.com>
2024-02-23 00:53:54 +0500

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -30,27 +30,6 @@ const Projects = () => {
       github: 'https://github.com/AbuTalha3/Todo-list',
       skills: ['HTML', 'CSS', 'React', 'Redux'],
     },
-    // {
-    //   id: 3,
-    //   title: 'React Metric App',
-    //   subtitle: 'React-csp-metric-app',
-    //   description: 'In this capstone project, I\'ve created an innovative Stock app offering in-depth numerical insights into the stock data of various active companies. The app features detailed company profiles, including performance metrics such as Market Capitalization and Profit Dividends. These metrics are updated in real-time at intervals of 1 minute, 15 minutes, 30 minutes, 1 hour, and daily stock prices. The app comprises multiple pages to facilitate seamless navigation and exploration of the stock data',
-    //   image: image5,
-    //   live: 'https://cheerful-haupia-208b4f.netlify.app/',
-    //   github: 'https://github.com/AbuTalha3/react-csp-metric-app',
-    //   skills: ['HTML', 'CSS', 'React', 'Redux', 'API'],
-    // },
-    // {
-    //   id: 4,
-    //   title: 'Restaurant App',
-    //   subtitle: 'Restaurant',
-    //   description: 'Our Restaurant is a website for the restaurant. With this app , customers can see the meals , they can like and comment on meals. All data is preserved thanks to Involvement API. This website is build by using JavaScript, Webpack and ES6.',
-    //   image: image1,
-    //   live: 'https://abutalha3.github.io/csp-js-module2/',
-    //   github: 'https://github.com/siddghosh108/CapstoneJS',
-    //   skills: ['HTML', 'CSS', 'React', 'Redux'],
-    // },
-    {
      {
       id: 5,
       title: 'Space Traveler',
@@ -61,17 +40,6 @@ const Projects = () => {
       github: 'https://github.com/AbuTalha3/react_group_project',
       skills: ['HTML', 'CSS', 'React', 'Redux'],
     },
-    },
-    // {
-    //   id: 6,
-    //   title: 'Awesome Books App',
-    //   subtitle: 'Awesome Books',
-    //   description: 'Awesome Books is a must-have app for book readers, made with Hyper Text Markup Language, Cascading Style Sheets, and JavaScript. You can have your own collection of books that you want to read, or you have read already. You can add books with Author details and can remove them when no more needed. An Awesome books store project, where you can enter the name and author of the books and have it stored, Developed using HTML, CSS, and Javascript',
-    //   image: image6,
-    //   live: 'https://abutalha3.github.io/AwesomeBooksList/',
-    //   github: 'https://github.com/AbuTalha3/AwesomeBooksList',
-    //   skills: ['HTML', 'CSS', 'JavaScript', 'WebPack'],
-    // },
   ]
 
     return (
